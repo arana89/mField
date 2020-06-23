152568ffccdd57bc3fbf630933b37570b056c123
arana89 <arjun.rana@gmail.com>
2020-06-23 11:18:52 -0700

    added mach number

--- example.m
+++ example.m
@@ -5,10 +5,17 @@ obj = mField();
 methods(obj)
 %% show available properties
 properties(obj)
-%% use loadOVF to get magnetization field of Hopfion and assign to mx,my,mz
+%% use loadOVF to get magnetization field of Hopfion 
 obj.loadOVF('./testData/hopfion_n25_r004_H1_z0.ovf');
 %%
 obj.showPlot = 1; %boolean to plot Hopfion
-obj.hopfNumber;
+obj.hopfNumber();
 %% print computed Hopf number
 obj.tHopfion
+%% assign physical quantities and compute mach number
+obj.Ms = 790e3;
+obj.Aex = 10e-12;
+obj.cellSize = 5e-9 * [1 1 1];
+
+obj.machNumber(); %assigned to obj.tMach
+
